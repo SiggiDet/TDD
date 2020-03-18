c58f657a9e2e1f0cfc49f60d9d7bc2b1f90e1720
Sigurður Detlef <siggidetjons6@gmail.com>
2020-03-18 21:55:09 +0000

    The Add method supports a single number

--- test_stringcalc.py
+++ test_stringcalc.py
@@ -4,5 +4,6 @@ def test_empty_string():
     assert string_calculator.add("") == 0
 
 def test_one_string():
-    """adds a string that is '1' and puts it in the add function in string_calculator"""
-    assert string_calculator.add("1") == 1
+    """recives a string of a number and returns it as an integer"""
+
+    assert string_calculator.add("465") == 465
