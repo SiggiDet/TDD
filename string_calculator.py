a13ce1a34857dcb012224337382f903cc2ca26c5
Sigurður Detlef <siggidetjons6@gmail.com>
2020-03-18 21:26:59 +0000

    The function is able to return 1 if the function recieves string that is '1'

--- string_calculator.py
+++ string_calculator.py
@@ -2,3 +2,5 @@ def add(number):
     """recieves a string and returns a value"""
     if number == "":
         return 0
+    elif number == "1":
+        return 1
--- test_stringcalc.py
+++ test_stringcalc.py
@@ -1,4 +1,8 @@
 import string_calculator
 def test_empty_string():
-    """recieves an empty string and puts it in the add function in string_calculator"""
+    """adds an empty string in the add function in string_calculator"""
     assert string_calculator.add("") == 0
+
+def test_one_string():
+    """adds a string that is '1' and puts it in the add function in string_calculator"""
+    assert string_calculator.add("1") == 1
