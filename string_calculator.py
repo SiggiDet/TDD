2054f2856a35cb239581fc7768cb379d045d68e9
Sigurður Detlef <siggidetjons6@gmail.com>
2020-03-23 22:10:25 +0000

    part 7, The Add method should raise an error (throw an exception) when the input contains a negative number

--- string_calculator.py
+++ string_calculator.py
@@ -1,7 +1,7 @@
-class NegativeNumberError(Exception):
+class NegativeNumError(Exception):
     pass
 
-def get_num(number):
+def get_num(number, def_type = None): # def_type is either negative or None (add)
     """Gets the number from the string, and cuts the string"""
     len_of_number = len(number)
     int_num = ""
@@ -10,9 +10,15 @@ def get_num(number):
 
     while i != len_of_number: # The while loop is used for if the numbers happens to be longer then one letter
         if number[i] == ",":
+            if def_type == "negative":
+                i += 1 # remove the ","
             break
+
         elif number[i] == "\n":
+            if def_type == "negative":
+                i += 1 # remove the "\n"
             break
+
         else:
             int_num += number[i]
             i += 1
@@ -20,16 +26,43 @@ def get_num(number):
     new_number = number[i:] # disposing the number that we collected from the string (number)
     return int_num,new_number # cuts the number (prev) away from the string (number)
 
+def negative_recur(negative_num_list,number):
+    len_of_number = len(number)
+    if len_of_number == 0:
+        negative_num_str = "Negatives not allowed: " #lists all the negative numbers
+
+        for i in range(len(negative_num_list)):
+            if i+1 == len(negative_num_list):
+                negative_num_str += str(negative_num_list[i])
+            else:
+                negative_num_str += str(negative_num_list[i]) + ", "
+
+        raise NegativeNumError(negative_num_str)
+
+    int_num, number = get_num(number,"negative")
+    if int(int_num) < 0:
+        negative_num_list.append(int(int_num))
+
+    negative_recur(negative_num_list,number)
+
+def negative(negative_num, number):
+    """Lists all the negative numbers from the string (number)"""
+    negative_num_list = [negative_num]
+    negative_recur(negative_num_list,number)
+
 def add_recur(number,prev= 0):
     """Recursive"""
     len_of_number = len(number)
 
     if len_of_number == 0:
         return 0 + prev
+
     elif number[0] == "\n":
         return add_recur(number[1:]) / prev
+
     elif number[0] == ",":
         return add_recur(number[1:]) + prev
+
     else:
         prev,number = get_num(number)
         prev = int(prev)
@@ -38,7 +71,8 @@ def add_recur(number,prev= 0):
             prev = prev - 1000
 
         elif prev < 0:
-            raise 
+            negative(prev,number[1:]) # when we try to find all of the negative numbers we want to skip "," and "\n"
+
         return add_recur(number,prev)
 
 def add(number):
--- test_stringcalc.py
+++ test_stringcalc.py
@@ -1,4 +1,6 @@
 import string_calculator
+import pytest
+
 def test_empty_string():
     """adds an empty string in the add function in string_calculator"""
     assert string_calculator.add("") == 0
@@ -14,8 +16,12 @@ def test_two_num_string():
 def test_three_num_string():
     assert string_calculator.add("1,2,3") == 6
 
-def test_much_unknown_argument():
+def test_unknown_argument():
     assert string_calculator.add("1,2,3,4,5,6,8,9,10,465") ==   513
 
 def test_more_then_thousand_argument():
     assert string_calculator.add("1001,2") == 3
+
+def test_negative_argument():
+    with pytest.raises(string_calculator.NegativeNumError):
+        string_calculator.add("-1,3,-2,5")
